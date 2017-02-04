d4f3ad8fec00bbf8c28092a61910a9346f5a9ea6
Hamid Habib <hamidhabib@hamidhabib.local>
2017-02-04 18:16:56 -0500

    root page changed to sign in

--- src/pages/account/signin/signin.ts
+++ src/pages/account/signin/signin.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { SignupPage } from "../signup/signup";
+import {TabsPage} from "../../tabs/tabs";
 
 @Component({
   selector: 'page-signin',
@@ -14,6 +15,11 @@ export class SigninPage {
   //Route to Sign-up page
   public createAccount() {
     this.navCtrl.push(SignupPage);
+  }
+
+  public signin(){
+    this.navCtrl.push(SigninPage);
+    this.navCtrl.setRoot(TabsPage);
   }
 
   ionViewDidLoad() {
