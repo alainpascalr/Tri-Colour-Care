35124b6cb494eef7407609ccc3c68cdfd2d715d0
Rob Cardy <rob.cardy.31@gmail.com>
2017-02-04 13:29:49 -0500

    more features added

--- lyff-server/server/serializers.py
+++ lyff-server/server/serializers.py
@@ -1,21 +1,22 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from server.models import Doctor, Patient, DoctorPatient
+from server.models import Comment, Doctor, Document, Patient, Medication, DoctorPatient
+
+
+class CommentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'doctor', 'patient', 'comment', 'private')
 
 
 class DoctorSerializer(serializers.ModelSerializer):
+    # TODO: Add hyperlink
+    # patients = 'http://localhost:8000/doctorpatient?doctor=' + Doctor.id
 
     class Meta:
         model = Doctor
         fields = ('url', 'id', 'firstname', 'lastname', 'specialty', 'photo',)
-
-
-class PatientSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Patient
-        fields = ('url', 'firstname', 'lastname', 'email', 'password', 'phone', 'age',
-                  'weight', 'height', 'healthcard', 'photo',)
 
 
 class DoctorPatientSerializer(serializers.ModelSerializer):
@@ -25,8 +26,31 @@ class DoctorPatientSerializer(serializers.ModelSerializer):
         fields = ('id', 'doctor', 'patient',)
 
 
+class DocumentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Document
+        fields = ('id', 'doctor', 'patient', 'file', 'private')
+
+
+class PatientSerializer(serializers.ModelSerializer):
+    # TODO: Add hyperlink
+    # doctors = 'http://localhost:8000/doctorpatient?patient=' + Patient.healthcard
+    class Meta:
+        model = Patient
+        fields = ('url', 'firstname', 'lastname', 'email', 'password', 'phone', 'age',
+                  'weight', 'height', 'healthcard', 'photo',)
+
+
+class MedicationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Medication
+        fields = ('id', 'doctor', 'patient', 'medication', 'dose')
+
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = User
-        fields = ('url', 'id', 'username',)
+        fields = ('url', 'id', 'username', )
--- lyff-server/server/views.py
+++ lyff-server/server/views.py
@@ -6,9 +6,10 @@ from rest_framework.response import Response
 from rest_framework import mixins
 from rest_framework import generics
 from rest_framework import permissions
-from server.models import Doctor, DoctorPatient, Patient
-from server.serializers import DoctorSerializer, DoctorPatientSerializer, PatientSerializer
+from server.models import Comment, Doctor, DoctorPatient, Document, Medication, Patient
+from server.serializers import CommentSerializer, DoctorSerializer, DoctorPatientSerializer, DocumentSerializer, MedicationSerializer, PatientSerializer
 from rest_framework import filters
+
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
@@ -20,9 +21,44 @@ class UserDetail(generics.RetrieveAPIView):
     serializer_class = UserSerializer
 
 
+class CommentList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    # permission_classes = permissions.IsAuthenticated
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+
+class CommentDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    # permission_classes = permissions.IsAuthenticated
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
 class DoctorList(mixins.ListModelMixin,
-               mixins.CreateModelMixin,
-               generics.GenericAPIView):
+                 mixins.CreateModelMixin,
+                 generics.GenericAPIView):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
     # permission_classes = permissions.IsAuthenticated
@@ -56,8 +92,8 @@ class DoctorDetail(mixins.RetrieveModelMixin,
 
 
 class DoctorPatientList(mixins.ListModelMixin,
-               mixins.CreateModelMixin,
-               generics.GenericAPIView):
+                        mixins.CreateModelMixin,
+                        generics.GenericAPIView):
     queryset = DoctorPatient.objects.all()
     serializer_class = DoctorPatientSerializer
     # filter_backends = filters.SearchFilter
@@ -75,11 +111,83 @@ class DoctorPatientList(mixins.ListModelMixin,
 
 
 class DoctorPatientDetail(mixins.RetrieveModelMixin,
-                   mixins.UpdateModelMixin,
-                   mixins.DestroyModelMixin,
-                   generics.GenericAPIView):
+                          mixins.UpdateModelMixin,
+                          mixins.DestroyModelMixin,
+                          generics.GenericAPIView):
     queryset = DoctorPatient.objects.all()
     serializer_class = DoctorPatientSerializer
+
+    # permission_classes = permissions.IsAuthenticated
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+class DocumentList(mixins.ListModelMixin,
+                   mixins.CreateModelMixin,
+                   generics.GenericAPIView):
+    queryset = Document.objects.all()
+    serializer_class = DocumentSerializer
+    # permission_classes = permissions.IsAuthenticated
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+
+class DocumentDetail(mixins.RetrieveModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.DestroyModelMixin,
+                     generics.GenericAPIView):
+    queryset = Document.objects.all()
+    serializer_class = DocumentSerializer
+    # permission_classes = permissions.IsAuthenticated
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+class MedicationList(mixins.ListModelMixin,
+                     mixins.CreateModelMixin,
+                     generics.GenericAPIView):
+    queryset = Medication.objects.all()
+    serializer_class = MedicationSerializer
+
+    # permission_classes = permissions.IsAuthenticated
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+
+class MedicationDetail(mixins.RetrieveModelMixin,
+                       mixins.UpdateModelMixin,
+                       mixins.DestroyModelMixin,
+                       generics.GenericAPIView):
+    queryset = Medication.objects.all()
+    serializer_class = MedicationSerializer
 
     # permission_classes = permissions.IsAuthenticated
 
@@ -135,4 +243,7 @@ def api_root(request, format=None):
         'doctors': reverse('doctor-list', request=request, format=format),
         'patients': reverse('patient-list', request=request, format=format),
         'doctorpatients': reverse('doctorpatient-list', request=request, format=format),
+        'medications': reverse('medication-list', request=request, format=format),
+        'documents': reverse('document-list', request=request, format=format),
+        'comments': reverse('comment-list', request=request, format=format),
     })
