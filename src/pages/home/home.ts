ba532fe48e594e6acac2f710fd8a576750705b05
Hamid Habib <hamidhabib@hamidhabib.local>
2017-02-04 14:54:46 -0500

    hardcoded data removed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,9 +7,44 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  doctor = [];
+  medication = [];
+  symptom = [];
+  user = [];
   constructor(public navCtrl: NavController) {
-
+    //doctor segment
+    this.doctor = [
+      {
+        'name': 'Dr. Tod',
+      }
+      ];
+    //medication segment
+    this.medication = [
+      {
+      'name': 'Advil',
+      },
+      {
+        'name': 'Tylenol',
+      }
+    ];
+    //symptom segment
+    this.symptom = [
+      {
+      'name': 'Chest pain',
+      },
+      {
+        'name': 'Headache',
+      }
+    ];
+    //user segment
+    this.user = [
+      {
+        'full_name' : 'Jennifer Aniston',
+        'age' : '23',
+        'condition' : 'critical',
+        'profile_pic' : 'http://kingofwallpapers.com/jennifer-aniston/jennifer-aniston-007.jpg'
+      }
+    ]
   }
 
 }
---
+++ src/pages/reason-for-visit/reason-for-visit.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/*
+  Generated class for the ReasonForVisit page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-reason-for-visit',
+  templateUrl: 'reason-for-visit.html'
+})
+export class ReasonForVisitPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ReasonForVisitPage');
+  }
+
+}
