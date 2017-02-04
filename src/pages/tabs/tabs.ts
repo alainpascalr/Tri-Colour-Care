b1ae4978689e37c9cbf2343a9f616908a6c6bda2
Hamid Habib <hamidhabib@hamidhabib.local>
2017-02-04 10:42:58 -0500

    profile added

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { HomePage } from '../home/home';
+import {ProfilePage} from "../profile/profile";
+
 
 @Component({
   templateUrl: 'tabs.html'
@@ -8,6 +10,7 @@ export class TabsPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
   homeRoot: any = HomePage;
+  profileRoot: any = ProfilePage;
   constructor() {
 
   }
